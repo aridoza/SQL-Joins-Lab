d127358cbffd82fda1407fb8881013efa7c81526
aridoza <mendozaarielr@gmail.com>
2019-10-10 15:59:14 -0400

    fixed run_queries.sh to run from terminal using sh command

--- run_queries.sh
+++ run_queries.sh
@@ -4,11 +4,6 @@
 # Employees working at macys
 # Companies in Boston
 # Employee with the highest salary
-
-BEGIN;
-SELECT first,last FROM employees INNER JOIN jobs ON employees.ssn = jobs.ssn WHERE jobs.company = 'Macy''s';
-
-SELECT company FROM jobs INNER JOIN employees ON employees.ssn = jobs.ssn WHERE employees.city = 'Boston';
-
-SELECT first,last FROM employees INNER JOIN jobs ON employees.ssn = jobs.ssn WHERE salary = (SELECT MAX(salary) FROM jobs);
-COMMIT;
+psql -d jobtracker -c "SELECT first,last FROM employees INNER JOIN jobs ON employees.ssn = jobs.ssn WHERE jobs.company = 'Macy''s'";
+psql -d jobtracker -c "SELECT company FROM jobs INNER JOIN employees ON employees.ssn = jobs.ssn WHERE employees.city = 'Boston'";
+psql -d jobtracker -c "SELECT first,last FROM employees INNER JOIN jobs ON employees.ssn = jobs.ssn WHERE salary = (SELECT MAX(salary) FROM jobs)";
